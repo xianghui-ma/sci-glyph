214f2cb7fba50a97c112508c2faf0bf1482eba94
unknown <ma_dong_liang@163.com>
2023-03-07 13:24:39 +0800

    init project

--- src/sciGlyph/primitive.js
+++ src/sciGlyph/primitive.js
@@ -1,7 +1,7 @@
 /*
  * @Author: Dongliang Ma
  * @Date: 2023-03-07 13:19:59
- * @LastEditTime: 2023-03-07 13:23:52
+ * @LastEditTime: 2023-03-07 13:24:01
  * @LastEditors: Dongliang Ma
  * @Description: 一个常用的可视化论文图标库
  * @GitHub: https://github.com/xianghui-ma
